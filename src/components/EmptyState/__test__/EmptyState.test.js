c9b2f3f7eeb3d0c25a21de231d20afeb0058a831
Barros <fellipe.a.barros@accenture.com>
2020-12-12 23:15:03 -0300

    add empty button and test

--- src/components/EmptyState/__test__/EmptyState.test.js
+++ src/components/EmptyState/__test__/EmptyState.test.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import EmptyState from '../EmptyState';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
 
 describe('Given EmptyState Component', () => {
+    const defaultProps = {
+        withGoBack: true
+    }
+    
     it("Renders EmptyState correctly", () => {
         const { getByTestId } = render(<EmptyState />);
         expect(getByTestId('EmptyState')).toBeTruthy();
         expect(getByTestId('EmptyStateText')).toBeTruthy();
-        expect(getByTestId('EmptyStateButton')).toBeTruthy();
+    })
+
+    it("Renders EmptyState with button", () => {
+        // const { getByTestId } = render(<EmptyState {...defaultProps} />);
+        const tree = renderer
+                        .create(
+                            <Router>
+                                <EmptyState {...defaultProps} />
+                            </Router>
+                        )
+                        .toJSON();
+        expect(tree).toBeTruthy();
+        expect(tree).toBeTruthy();
+        expect(tree).toBeTruthy();
     })
 })
